3d91bf76dab673c3fff9f198d6a2545ba85700ad
Kamron Hays <kamron.hays@live.com>
2017-05-10 17:43:38 -0400

    completed timer implementation

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,15 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def time_string()
+    hours = @seconds / (60 * 60)
+    remainder = @seconds % (60 * 60)
+    minutes = remainder / 60
+    seconds = remainder % 60
+    '%02i:%02i:%02i' % [hours, minutes, seconds]
+  end
+  
+  def initialize
+    @seconds = 0
+  end
 end
