4dc49a1bc0c213ca28bff0992e11bfcb113c6423
Kamron Hays <kamron.hays@live.com>
2017-05-10 17:40:05 -0400

    completed book titles implementation

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,22 @@
 class Book
-# write your code here
+  attr_accessor :title
+  
+  # Assign a title (via operator override)
+  def title=(phrase)
+    exceptions = ['the', 'a', 'an', 'and', 'in', 'of']
+    words = phrase.split # splits on space by default
+    first = true
+    words.each do |word|
+      if !exceptions.include?(word) || first
+        word.capitalize! # exclamation assigns result to 'word'
+      end
+      first = false
+    end
+    @title = words.join(' ') #put the phrase back together
+  end
+
+  # Get the title.
+  def title
+    @title
+  end
 end
